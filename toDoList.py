a85e8cda4c6e5555de63163f045de48586011b93
Vijay6868 <saroyav3@gmail.com>
2023-08-26 14:45:46 +1200

    basic functionalities are working at this stage

--- toDoList.py
+++ toDoList.py
@@ -1,40 +1,51 @@
 import datetime
+import os
+os.system('clear')
 
-print("Welcome to 'TEXT USER INETERFACE' of To do List app")
 def tuiInstructions():
     print("ENTER 'A' TO ADD A TASK TO THE LIST")
     print("ENTER 'V' TO VIEW YOUR TASKS")
     print("ENTER 'Q' TO EXIT")
+    
+def setTask():
+    taskIn  = input('Enter your task name : ')
+    task.append(taskIn)
+def setDate():
+    date.append(datetime.datetime.now())
+
+def setTaskID(taskId):
+    task_id.append(taskId)
+
+print("Welcome to 'TEXT USER INETERFACE' of To do List app") 
+print("_____________________________________________________")
 
 task_id =[]
 task =[]
 date=[]
-#def setTaskid(taskId):
 
-def setTask():
-    taskIn  = input('Enter your task')
-    task.append(taskIn)
-def setDate():
-    date.append(datetime.datetime.now())
-#def add_task():
-def setTaskID(taskId):
-    task_id.append(taskId)
+user = 'g'
 
-tuiInstructions()
-user = input()
-
-while(user!='q' or user!='Q'):
-    
-    if(user=='A'or user=='a'):
+while user.lower()!='q':
+    tuiInstructions()
+    user = input()
+    if(user.lower() =='a'):
         setTask()
         setDate()
-        setTaskID(len(setTask))
-    elif(user=='V'or user=='v'):
+        setTaskID((len(task)))
+        
+
+    elif(user.lower()=='v'):
         for i in range(0,len(task_id)):
-            print("Task ID:"+task_id[i]+"\nTask Date: "+date[i]+"\nTask:"+task)
-            print("###########################################################")
+            print("Task ID:"+str(task_id[i])+"\nTask Date: "+str(date[i])+"\nTask:"+task[i])
+            print("_____________________________________________________________________")
+            
+    elif(user.lower=='q'):
+        break
+
     else:
         print("Wrong input")
+    
+   
 
     
 
